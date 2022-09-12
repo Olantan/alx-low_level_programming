a09637654785c2ba8cbbb2f4d1c32f0e9294c02a
Olantan <110933448+Olantan@users.noreply.github.com>
2022-09-12 19:55:29 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,21 @@
-/*C program to print all upper case and lower case alphabets.*/
- 
+
 #include <stdio.h>
- 
-int main()
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-    char i;
- 
-    printf("Capital (upper) case characters:\n");
-    for(i='A'; i<='Z'; i++)
-        printf("%c ",i);
- 
-    printf("\n\nLower case characters:\n");
-    for(i='a'; i<='z'; i++)
-        printf("%c ",i);
-     
-    return 0;
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
 }
