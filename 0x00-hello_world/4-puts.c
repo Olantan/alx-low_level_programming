e57dc7dd2ab49ff81102866470dbae7abe4e8000
Olantan <110933448+Olantan@users.noreply.github.com>
2022-09-12 19:26:40 +0300

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+ 1   // Fig. 2.1: fig02_01.c
+ 2   // A first program in C.
+ 3   #include <stdio.h>
+ 4
+ 5   // function main begins program execution
+ 6   int main( void )
+ 7   {
+ 8      puts( "Programming is like building a multilingual puzzle,\n" );
+ 9   } // end function main
