e8d95a393793317fc8a4a7d426d5d67eece0fe69
Olantan <110933448+Olantan@users.noreply.github.com>
2022-09-13 07:42:14 +0300

    Update 0-main.c, main.h

--- 0x03-debugging/0-main.c, main.h
+++ 0x03-debugging/0-main.c, main.h
@@ -1,19 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
+        i = 98;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
+
 #ifndef HOLBERTON_H 
  #define HOLBERTON_H 
   
