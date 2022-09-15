f78458deb0af8c75730dd4a74b22bcedeeae0b27
Olantan <110933448+Olantan@users.noreply.github.com>
2022-09-15 22:05:14 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "putchar.h"
+#include "holberton.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */ 
- int main(void) 
- { 
-         char *sh = "Putchar"; 
-  
-         while (*sh) 
-         { 
-                 _putchar(*sh); 
-                 sh++; 
-         } 
-         _putchar('\n'); 
-  
-         return (0); 
- }
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
