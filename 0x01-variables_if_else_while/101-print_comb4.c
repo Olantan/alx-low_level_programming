2c3d0ed2ab798b888b98e67328fdd4abe95ce528
Olantan <110933448+Olantan@users.noreply.github.com>
2022-09-16 06:09:18 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,34 @@
-
 #include <stdio.h>
 
-int main()
+/**
+ * main - Function
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    //1
-    int i, j, k;
-    int arr[3];
-    //2
-    printf("Enter the first number : ");
-    scanf("%d", &arr[0]);
-    printf("Enter the second number : ");
-    scanf("%d", &arr[1]);
-    printf("Enter the third number : ");
-    scanf("%d", &arr[2]);
-    //3
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            for (k = 0; k < 3; k++)
-            {
-                if (i != j && j != k && k != i)
-                {
-                    //4
-                    printf("[%d %d %d]\n", arr[i], arr[j], arr[k]);
-                }
-            }
-        }
-    }
+	int d1, d2, d3
+
+	for (d1 = 0; d1 < 8; d1++)
+	{
+		for (d2 = d1 + 1; d2 < 9; d2++)
+		{
+			for (d3 = d2 + 1; d3 < 10; d3++)
+			{
+				putchar((d1 % 10) + '0');
+				putchar((d2 % 10) + '0');
+				putchar((d3 % 10) + '0');
+
+				if (d1 == 7 && d2 == 8 && d3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
