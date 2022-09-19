53beffc90688018e2b8f28f57f25059ea3ca4861
Olantan <110933448+Olantan@users.noreply.github.com>
2022-09-19 09:49:26 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,11 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-}
+#include "holberton.h" 
+  
+ /** 
+  * reset_to_98 - main 
+  * @n: - pinter to int 
+  */ 
+  
+ void reset_to_98(int *n) 
+ { 
+ *n = 98; 
+ }
