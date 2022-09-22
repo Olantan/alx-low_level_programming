044f9305fe7f068ccf2c122c05640a1ab3103f7a
Olantan <110933448+Olantan@users.noreply.github.com>
2022-09-22 09:53:30 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,32 +1,26 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * _strncat - function
- *@dest: char
- *@src: char
- *@n: int
- *Return: char
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-char *_strncat(char *dest, char *src, int n)
+int main(void)
 {
-	int i, j;
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
 
-	i = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	j = 0;
-	while (src[j] != '\0' && j < n)
-	{
-	dest[i] = src[j];
-	i++;
-	j++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
