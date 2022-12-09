d95d06f62712286d84f7104c6f93b690ca0073dd
Olantan <110933448+Olantan@users.noreply.github.com>
2022-12-09 23:40:38 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,28 +1,32 @@
+/*
+ * File: 102-print_comb5.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Printing combinations of two two-digit numbers.
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int nb1, nb2;
+	int num1, num2;
 
-	for (nb1 = 0; nb1 <= 98; nb1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (nb2 = nb1 + 1; nb2 <= 99; nb2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar((nb1 / 10) + '0');
-			putchar((nb1 % 10) + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar((nb2 / 10) + '0');
-			putchar((nb2 % 10) + '0');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (nb1 == 98 && nb2 == 99)
-				break;
+			if (num1 == 98 && num2 == 99)
+				continue;
 
 			putchar(',');
 			putchar(' ');
