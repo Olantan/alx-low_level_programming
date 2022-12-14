9bfcb63c0e39112172f380b6d6125685cf7eb785
Olantan <110933448+Olantan@users.noreply.github.com>
2022-12-14 22:58:39 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,28 @@
+/*
+ * File: 8-24_hours.c
+ * Auth: Olantan
+ */
+
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
-int main(void)
+void jack_bauer(void)
 {
-    jack_bauer();
-    return (0);
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
