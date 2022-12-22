7f4a7799b83cf5136ad8142f47ca28cddd5f6a15
Olantan <110933448+Olantan@users.noreply.github.com>
2022-12-23 01:54:29 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,21 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
- */
-char *rot13(char *s)
-{
-	int i = 0;
 
-	while (s[i] != '\0')
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
-		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
-		}
-		i++;
+		_putchar('-');
+		num = -num;
 	}
-	return (s);
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
